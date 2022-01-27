18bea642e85ce895ae716d1b78631f0f2d98e121
Roberto <roberto.cerdan@ikasle.egibide.org>
2022-01-27 09:24:11 +0100

    implementacion-email

---
+++ app/Http/Controllers/MailControler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MailIncidencia;
+
+class MailControler extends Controller
+{
+    public function sendEmailIncidencia(Request $request)
+    {
+
+        $email=$request->mail;
+        $contenido=$request->contenido;
+        $details = [
+            'title' => 'Nueva incidencia',
+            'body'=>$contenido
+        ];
+        /*$email=[
+            'roberto.cerdan@ikasle.egibide.org',
+            'barbara.lopez@ikasle.egibide.org',
+            'raul.melgosa@ikasle.egibide.org'
+        ];
+        */
+
+        Mail::to($email)->send(new MailIncidencia($details));
+        return "Correo enviado";
+    }
+    public function sendEmailResuelta($email, $contenido)
+    {
+        $details = [
+            'title' => 'Incidencia resuelta',
+            'body'=>$contenido
+            ];
+        /*
+        $email=[
+            'roberto.cerdan@ikasle.egibide.org',
+            'barbara.lopez@ikasle.egibide.org',
+            'raul.melgosa@ikasle.egibide.org'
+        ];
+        */
+        Mail::to($email)->send(new MailIncidencia($details));
+        return "Correo enviado";
+    }
+}
---
+++ resources/views/email/incidencia.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Igobide ascensores</title>
+    </head>
+    <body>
+        <h2>{{ $details ['title'] }}</h2>
+        <p>{{ $details ['body'] }}</p>
+    </body>
+</html>
