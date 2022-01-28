3d5b1583ea007d90e1e05c2bca39aa0344de1b26
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2022-01-28 10:49:09 +0100

    pruebas

--- resources/views/formularios/formularioOperador.blade.php
+++ resources/views/formularios/formularioOperador.blade.php
@@ -71,30 +71,18 @@
                 <div class="col-12 col-sm-11 col-md-10  border border-light "></div>
                 <div class="col-12 col-sm-11 col-md-10  mb-3">
                     <h2>Incidencia</h2>
-                    <div class="row g-0 col-10">
-                        <label for="select-tecnico">T&eacute;cnico</label>
-                        <select class="form-select d-inlineblock" name="" id="select-tecnico">
-                    
-                        <!-- 
-                            foreach()
-                                <option value=""></option>
-                            endforeach
-                        -->
-                        </select>
-                    </div>
                 </div>
 
                 <div class="col-12 col-sm-11 col-md-10 ">
                    <div class="row justify-content-center"> 
                        <div class="col-6 col-sm-6 col-md-5 col-lg-4 form-switch mb-3">
-                            <label class="me-3" for="urgente" >Urgente</label>
-                            <input type="checkbox" id="urgente" name="urgente" value="true">
+                            <label class="me-3 form-check-label" for="urgente" >Urgente</label>
+                            <input class="form-check-input" type="checkbox" id="urgente" name="urgente" value="true">
                         </div>
 
-                        <div class="col-6 col-sm-6 col-md-5 col-lg-4  mb-3">     
+                        <div class="col-6 col-sm-6 col-md-5 col-lg-4  mb-3 row g-0">     
                             <label class="me-3" for="select-tecnico">T&eacute;cnico</label>
                             <select class="form-select d-inlineblock" name="" id="select-tecnico">
-                        
                             <!-- 
                                 foreach()
                                     <option value=""></option>
