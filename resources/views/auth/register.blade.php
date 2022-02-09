556f3b6542662bde0272505260323cc7f2769d85
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2022-02-09 13:22:57 +0100

    TODO

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('crearUsuario') }}">
+<div class="container d-flex justify-content-center">
+<img class="position-absolute m-auto" style="max-width: 100%; max-height: 100vh; top:0px; z-index: -1;" src="{{asset('/img/Abstract-3d-art-background.png')}}" alt="">
+    <div class="col-md-7 col-sm-8 col-11 row justify-content-center align-items-center m-auto shadow mb-5 rounded">
+                    <form class="col-12 m-auto justify-content-center bg-light py-5 rounded" style="--bs-bg-opacity: .6;" method="POST" action="{{ route('crearUsuario') }}">
                         @csrf
-
+                        <h1 class="text-center"><b>Registrar Usuarios</b></h1>
                         <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Nombre de usuario') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-end"><b>{{ __('Nombre de usuario') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
@@ -26,7 +22,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nombre') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end"><b>{{ __('Nombre') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -40,7 +36,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="apellidos" class="col-md-4 col-form-label text-md-end">{{ __('Apellidos') }}</label>
+                            <label for="apellidos" class="col-md-4 col-form-label text-md-end"><b>{{ __('Apellidos') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="apellidos" type="text" class="form-control @error('apellidos') is-invalid @enderror" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus>
@@ -54,7 +50,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end"><b>{{ __('Email') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -68,7 +64,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end"><b>{{ __('Contraseña') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -82,7 +78,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirmar contraseña') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end"><b>{{ __('Confirmar contraseña') }}</b></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -90,7 +86,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="rol" class="col-md-4 col-form-label text-md-end">{{ __('Rol') }}</label>
+                            <label for="rol" class="col-md-4 col-form-label text-md-end"><b>{{ __('Rol') }}</b></label>
 
                             <div class="col-md-6">
                                 <select name="rol" id="rol" class="form-select">
@@ -105,7 +101,7 @@
                         </div>
                         @if(auth()->user()->rol=='admin')
                         <div class="row mb-3">
-                            <label for="equipo" class="col-md-4 col-form-label text-md-end">{{ __('Equipo') }}</label>
+                            <label for="equipo" class="col-md-4 col-form-label text-md-end"><b>{{ __('Equipo') }}</b></label>
 
                             <div class="col-md-6">
                                 <select name="equipo_id" id="equipo" class="form-select">
@@ -116,7 +112,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="equipo" class="col-md-4 col-form-label text-md-end">{{ __('Zona del equipo') }}</label>
+                            <label for="equipo" class="col-md-4 col-form-label text-md-end"><b>{{ __('Zona del equipo') }}</b></label>
 
                             <div class="col-md-6">
                                 <select name="zona" id="rol" class="form-select">
@@ -135,9 +131,6 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
+                    </div>
 </div>
 @endsection
