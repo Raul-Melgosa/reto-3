9be5315927ffd4141d7d8513311b30ae45f8824d
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2022-02-09 10:26:24 +0100

    mix->asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
     @yield('assets')
     
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
